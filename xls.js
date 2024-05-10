31223863669d091fb6dfeffb5421acf104c2b53e
Won Park <parkwon@sysnova.co.kr>
2024-05-10 12:03:36 +0900

    demo added

--- xls.js
+++ xls.js
@@ -1,10 +1,8 @@
-var express = require('express');
-var router = express.Router();
-
+const e = require('express');
 const xlsx = require('xlsx');
 
 // Read the Excel file
-const workbook = xlsx.readFile('demo.xlsx');
+const workbook = xlsx.readFile('./demo.xlsx');
 
 // Get the first worksheet
 const worksheet = workbook.Sheets[workbook.SheetNames[0]];
@@ -58,6 +56,10 @@ const template = {
   templateManual: false,
 };
 
+// Print the data
+const renew = data.map((d) => updateValue(d));
+console.log(renew);
+
 function updateValue(t) {
   let s = JSON.parse(JSON.stringify(template));
 
@@ -88,64 +90,3 @@ function updateValue(t) {
 
   return s;
 }
-
-// Print the data
-const json = data.map((d) => updateValue(d));
-
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send(json);
-});
-
-module.exports = router;
-
-/*
-const json = {
-  responseMessage: 'The request has been completed',
-  responseCode: '200',
-  customBatchId: null,
-  labelList: [
-    {
-      labelCode: '07D6E1D4B199',
-      networkStatus: true,
-      articleList: [
-        {
-          articleId: 'C129060',
-          articleName: '클리오 샤프쏘심플브로우펜슬 1/2/3',
-          data: null,
-        },
-      ],
-      updateStatus: 'SUCCESS',
-      battery: 'GOOD',
-      signal: 'EXCELLENT',
-      type: 'NEWTON_GRAPHIC_2_2_RED_NFC',
-      gateway: {
-        id: 3500126,
-        code: 'D02544FFFE20D763',
-        macAddress: 'D0-25-44-20-D7-63',
-        ipAddress: '10.176.247.110',
-        name: 'GW_D02544FFFE20D763',
-        version: 'N1.3.7.0',
-        status: 'CONNECTED',
-        lastConnectionDate: '2024-05-07T14:38:52.328+0900',
-        port: 80,
-      },
-      firmwareVersion: 23,
-      templateName: ['OY_2_2_교차_240116.xsl'],
-      templateType: ['OY_2_2_교차'],
-      lastResponseTime: '2024-05-07T10:01:59.000+0900',
-      lastConnectionTime: '2024-05-01T04:04:16.203+0900',
-      arrow: null,
-      addInfo2: null,
-      addInfo3: null,
-      addInfo4: null,
-      addInfo5: null,
-      temperature: 26,
-      requestDate: '2024-05-01T04:04:16.025+0900',
-      completedDate: '2024-05-01T04:20:59.000+0900',
-      batteryLevel: 'FF',
-      templateManual: true,
-    },
-  ],
-};
-*/
